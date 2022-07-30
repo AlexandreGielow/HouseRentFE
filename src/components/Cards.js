c5c9b83ce8b9cca945ceb1b2f8f04b645f64b1e8
Alexandre Gielow <alexandregielow@senior.com.br>
2022-07-29 21:43:59 -0300

    add map

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,19 +3,32 @@ import { FetchProperties } from './properties/FetchProperties'
 import './Cards.css'
 import './Button.css'
 import './CardItem.css'
+import Maps from './Maps'
 
 function Cards () {
   return (
-    <div className='cards'>
-      <h1>Check out our best homes!</h1>
-      <div className='locationFilter' >
-        <div data-icon="search">lupinha</div>
-        <div className='cityFilter' placeholder='Search cities and areas'>
-
+    <div>
+      <div>
+        <h1>Check out our best homes!</h1>
+      </div>
+      <div className='mainHomes'>
+        <div className='listHouses'>
+          <div>
+            <div className='locationFilter' >
+              <div data-icon="search">
+            </div>
+          <div className='cityFilter' placeholder='Search cities and areas'></div>
+        </div>
+        </div>
+          <div>
+            <FetchProperties/>
+          </div>
+        </div>
+        <div className='maps'>
+          <Maps/>
         </div>
       </div>
-              <FetchProperties/>
-      </div>
+</div>
   )
 }
 
